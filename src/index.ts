35941d57cfac4d1d2959b6b00e21a5ab639bb760
Bartek F <bartekfr@wp.pl>
2024-04-29 12:11:08 +0200

    Add tiktoken package

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { OpenAI} from 'openai'
+import { encoding_for_model } from 'tiktoken'
 
 const openai = new OpenAI()
 
@@ -7,10 +8,17 @@ async function main() {
     model: 'gpt-3.5-turbo',
     messages:[{
         role: 'user',
-        content: 'How tall is mount Everest?'
+        content: 'How tall is mount Giewont?'
     }]
   })
   console.log(response.choices[0].message.content)
 }
 
+function encodePropmpt(prompt: string) {
+  const encoder = encoding_for_model('gpt-3.5-turbo')
+  const words = encoder.encode(prompt)
+  console.log(words)
+}
+
+// encodePropmpt('How tall is mount Everest?')
 main()
